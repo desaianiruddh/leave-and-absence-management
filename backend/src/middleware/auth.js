4bd260f5314ba9fa6f536da41c8e578613acabbd
desaianiruddh <aniruddh.d@simformsolutions.com>
2026-09-17 12:10:45 +0530

    changed favicon and removed console

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -16,7 +16,6 @@ exports.authenticateToken = (req, res, next) => {
 exports.requireRole =
   (...roles) =>
   (req, res, next) => {
-    console.log('req', req);
     if (!roles.includes(req.user?.role)) {
       return res.status(403).json({ error: 'Unauthorized' });
     }
